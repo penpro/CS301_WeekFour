490ab89d1d1dc438df3fb7c38932ec467b1a24c4
penpro <penumbraproductionsllc@gmail.com>
2025-10-19 21:13:19 -0700

    finished soda machine

---
+++ SodaMachine.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SodaMachine {
+
+    private ArrayList<String> machineContents = new ArrayList<>();
+    private float cashInMachine;
+    private ArrayList<Integer> inventory = new ArrayList<>();
+    private ArrayList<Double> prices = new ArrayList<>();
+    private float coinsPending;
+
+    public SodaMachine(){
+        this.cashInMachine = 0.0f;
+        this.coinsPending = 0.0f;
+        this.machineContents.addFirst("Dr. Pepper");
+        this.inventory.addFirst(10);
+        this.prices.addFirst(0.75);
+    }
+
+    public void insertCoin(String coinName){
+        String LocalName = coinName.toLowerCase();
+        switch (LocalName){
+            case "quarter"  : setCoinsPending(0.25f);
+                System.out.println(randomCoinInsertMessage() + String.format("%.2f", coinsPending));
+                break;
+            case "nickel"   : setCoinsPending(0.05f);
+                System.out.println(randomCoinInsertMessage() + String.format("%.2f", coinsPending));
+                break;
+            case "penny"    : setCoinsPending(0.01f);
+                System.out.println(randomCoinInsertMessage() + String.format("%.2f", coinsPending));
+                break;
+            case "dime"     : setCoinsPending(0.10f);
+                System.out.println(randomCoinInsertMessage() + String.format("%.2f", coinsPending));
+                break;
+            default:
+                System.out.println("Your coin was rejected and plops into the coin return with a \"plink\"");
+        }
+    }
+
+    private void setCoinsPending(float dollarDecimal){
+        coinsPending += dollarDecimal;
+    }
+
+    public Double GetSelectionPrice(int selection){
+        return prices.get(selection);
+    }
+
+    public String GetSelectionName(int selection){
+        return machineContents.get(selection);
+    }
+
+    public void BuySoda(int selection){
+        int available = inventory.get(selection);
+        if (coinsPending >= GetSelectionPrice(selection) && available >=1){
+            coinsPending -= GetSelectionPrice(selection);
+            inventory.set(selection, (available-1) );
+            cashInMachine += GetSelectionPrice(selection);
+            System.out.println("The machine whirs to life lamely and just when you think it ate your money you here a can drop into the shoot.");
+        }
+        else if (available < 1) {
+            System.out.println("The machine whirs to life lamely and just when you think it ate your money you see the \"Please Make Another Selection\" text scrawl across the display.");
+        }
+        if (coinsPending > 0){
+            System.out.println("Your $" + String.format("%.2f",coinsPending) + " clinks down into the coin return.");
+            coinsPending = 0;
+        }
+    }
+
+    public void checkInventory(){
+        for (int i = 0 ; i < machineContents.size(); i++){
+            System.out.println(machineContents.get(i) + "   QTY:   " + inventory.get(i));
+        }
+    }
+
+    private String randomCoinInsertMessage(){
+        Random roll = new Random();
+        float thisRoll = roll.nextFloat();
+        if (thisRoll < 0.35f){
+            return "Your coin rattles inside the machine and the display reads :  $";
+        }
+        else if (thisRoll < 0.65f) {
+            return "You can't hear your coin go in but the display reads :  $";
+        }
+        else if (thisRoll < 0.95) {
+            return "Your coin gets stuck for a second but you bump the machine and the display eventually reads :  $";
+        }
+        return "Your coin pops out the coin return but the display eventually reads :  $";
+    }
+
+    public void checkCashInMachine(){
+        System.out.println("You open up the machine and find $" + String.format("%.2f",cashInMachine));
+    }
+
+    public void setInventory(int selection, int qty){
+        this.inventory.set(selection, qty);
+    }
+
+
+
+
+
+    // insertcoin DONE
+    // getchange
+    // buy
+}
--- Tester.java
+++ Tester.java
@@ -1,12 +1,27 @@
 public class Tester {
     public static void main(String[] args) {
-        DateObj BirthDate = new DateObj(1982,3,19);
+        //DateObj BirthDate = new DateObj(1982,3,19);
 
-        System.out.println(BirthDate.toString());
-        System.out.println(BirthDate.GetDayOfTheWeekName());
+        //System.out.println(BirthDate.toString());
+        //System.out.println(BirthDate.GetDayOfTheWeekName());
 
-        BirthDate.SetNote("It's my birthday!");
+        //BirthDate.SetNote("It's my birthday!");
 
-        System.out.println(BirthDate.GetNote());
+        //System.out.println(BirthDate.GetNote());
+
+        SodaMachine testermachine = new SodaMachine();
+        testermachine.checkInventory();
+        testermachine.insertCoin("quarter");
+        testermachine.insertCoin("quarter");
+        testermachine.insertCoin("quarter");
+        testermachine.insertCoin("quarter");
+        testermachine.BuySoda(0);
+        testermachine.checkInventory();
+        testermachine.checkCashInMachine();
+        testermachine.setInventory(0,0);
+        testermachine.insertCoin("quarter");
+        testermachine.insertCoin("quarter");
+        testermachine.insertCoin("quarter");
+        testermachine.BuySoda(0);
     }
 }
